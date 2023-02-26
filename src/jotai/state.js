60d62cf5abe2fadefc58bc403b22fdbb26693cb1
Jaesin Cho <1005jsc@naver.com>
2023-02-27 01:49:57 +0900

    url 다시 확인

--- src/jotai/state.js
+++ src/jotai/state.js
@@ -6,4 +6,4 @@ export const contentSelectedAtom = atomWithReset(ContentsRouterData);
 // 기업 가입 리스트
 
 export const getCompanyMembershipDataAtom = atomWithReset([]);
-export const getCompanyMembershipDataListAtom = atomWithReset([]);
+// export const getCompanyMembershipDataListAtom = atomWithReset([]);
