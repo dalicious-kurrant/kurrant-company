7a188a81b3f78e7fb3e830548b0cf8f1f0819d92
Jaesin Cho <1005jsc@naver.com>
2023-03-01 20:03:07 +0900

    [유저정보, 기업가입 리스트 리뉴얼] 일단 다 지우기

--- src/components/Contents/CompanyMembership/CompanyMembership.jsx
+++ src/components/Contents/CompanyMembership/CompanyMembership.jsx
@@ -1,48 +1,18 @@
-import axios from 'axios';
 import ExcelTest from 'common/excel/ExcelTest';
-import Pagination from 'common/Pagination/Pagination';
-import usePagination from 'common/Pagination/usePagination';
-import Table from 'common/Table/Table';
+
 import useCompanyMembershipQuery from 'hooks/ReactQueryHooks/useCompanyMembershipQuery';
 
 import {useAtom} from 'jotai';
 import {exelCompanyMembershipAtom} from 'jotai/compayMembership';
-import {getCompanyMembershipDataListAtom} from 'jotai/state';
-import {getCompanyMembershipDataAtom} from 'jotai/state';
 
 import React from 'react';
-import {useState} from 'react';
-import {useEffect} from 'react';
-import {useQuery} from 'react-query';
 
 import styled from 'styled-components';
-import {CompanyMembershipFields} from './CompanyMembershipData';
-import useDataRender from './useHooks/useDataRender';
+
 const CompanyMembership = ({}) => {
   const [plan, setPlan] = useAtom(exelCompanyMembershipAtom);
 
-  const [companyMembershipList, setCompanyMembershipList] = useAtom(
-    getCompanyMembershipDataAtom,
-  );
-
   const {dataList, editMutate, submitExelMutate} = useCompanyMembershipQuery();
-
-  useEffect(() => {
-    setCompanyMembershipList(dataList);
-  }, [dataList]);
-
-  const [memoData, setMemoData] = useState(undefined);
-
-  const handleMemoChange = data => {
-    setMemoData(data);
-  };
-
-  useEffect(() => {
-    if (memoData) {
-      editMutate(memoData);
-    }
-  }, [memoData]);
-  const {status, isLoading} = useDataRender();
 
   // if (isLoading)
   //   return (
@@ -64,19 +34,7 @@ const CompanyMembership = ({}) => {
   return (
     <Container>
       <ExcelTest submitExelMutate={submitExelMutate} />
-      {plan.length < 1 && (
-        <>
-          {Array.isArray(companyMembershipList) &&
-          companyMembershipList.length !== 0 ? (
-            <Table
-              fieldsInput={CompanyMembershipFields}
-              dataInput={companyMembershipList}
-            />
-          ) : (
-            <div>아직 등록된 데이터가 없습니다. 데이터를 추가해주세요</div>
-          )}
-        </>
-      )}
+      {plan.length < 1 && <></>}
     </Container>
   );
 };
