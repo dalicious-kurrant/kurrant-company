35ec0488c721419f88d08ee3a7fc59323f74d4ba
jaesincho <jaesin.cho@dalicious.co>
2023-03-06 20:19:09 +0900

    페이지 상태 메세지 변경 2

--- src/components/Contents/CompanyMembership/CompanyMembership.jsx
+++ src/components/Contents/CompanyMembership/CompanyMembership.jsx
@@ -140,7 +140,7 @@ const CompanyMembership = ({}) => {
   if (companyMembershipData.length < 1)
     return (
       <>
-        <div>들어있는 데이터가 아직 없습니다. 데이터를 추가해주세요.</div>{' '}
+        <div>데이터가 아직 없습니다. 데이터를 추가해주세요.</div>{' '}
       </>
     );
 
--- src/components/Contents/UserStatus/UserStatus.jsx
+++ src/components/Contents/UserStatus/UserStatus.jsx
@@ -45,7 +45,7 @@ const UserStatus = () => {
   if (userStatusData.length < 1)
     return (
       <>
-        <div>들어있는 데이터가 아직 없습니다. 데이터를 추가해주세요.</div>{' '}
+        <div>데이터가 아직 없습니다. 데이터를 추가해주세요.</div>{' '}
       </>
     );
 
