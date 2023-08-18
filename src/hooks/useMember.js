ca3b358cd98643731c82761676cb4289a6a51bf6
jeongho8846 <dede8846@gmail.com>
2023-08-18 16:03:03 +0900

    멤버변경

--- src/hooks/useMember.js
+++ src/hooks/useMember.js
@@ -1,18 +1,29 @@
 import {memberApis} from 'api/member';
+import {userapi} from 'api/member';
 import {useMutation, useQuery, useQueryClient} from 'react-query';
 
-export function useGetMemberList() {
-  const groupCode = localStorage.getItem('code');
-  return useQuery('memberList', () => {
-    return memberApis.loadMemberList(groupCode);
+export function useUserLoad(page) {
+  return useQuery('getUserList', () => {
+    return userapi.getUserList(page);
   });
 }
 
-export function useDeleteMember() {
+export function useDeleteUser() {
   const queryClient = useQueryClient();
-  return useMutation(data => memberApis.deleteMember(data), {
+  return useMutation(data => userapi.deleteCheckedMember(data), {
     onSuccess: () => {
-      queryClient.invalidateQueries('memberList');
+      queryClient.invalidateQueries('getUserList');
+      alert('삭제 완료했습니다.');
+    },
+  });
+}
+
+export function useChangeMemo() {
+  const queryClient = useQueryClient();
+  return useMutation(data => userapi.changeMemoPost(data), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('getUserList');
+      alert('수정 완료했습니다.');
     },
   });
 }
